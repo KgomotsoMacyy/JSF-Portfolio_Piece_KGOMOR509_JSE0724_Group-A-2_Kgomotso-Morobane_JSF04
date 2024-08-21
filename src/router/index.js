f9ed80e589dfe191938153c340624dd6e99b88b9
Kgomotso Morobane <111234398+KgomotsoMacyy@users.noreply.github.com>
2024-08-21 15:57:30 +0200

    immported cart

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue'; // Path to Home.vue
 import ProductDetail from '../views/ProductDetails.vue'; // Path to ProductDetails.vue
 import Login from '../components/Login.vue'; // Path to Login.vue
+import Cart from '../views/Cart.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/product/:id', component: ProductDetail, props: true },
   { path: '/login', component: Login },  // Add the login route
+  { path: '/cart', component: Cart },
 ];
 
 const router = createRouter({
