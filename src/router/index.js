a083b5a3840bf24eed42cc54c08bc32a855298a6
Kgomotso Morobane <111234398+KgomotsoMacyy@users.noreply.github.com>
2024-08-21 09:34:05 +0200

    Add the path to the product details and the login

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue'; // Path to Home.vue
-import ProductDetail from '../views/ProductDetails.vue';
- // Path to ProductDetail.vue
+import ProductDetail from '../views/ProductDetails.vue'; // Path to ProductDetails.vue
+import Login from '../components/Login.vue'; // Path to Login.vue
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/product/:id', component: ProductDetail, props: true }
+  { path: '/product/:id', component: ProductDetail, props: true },
+  { path: '/login', component: Login },  // Add the login route
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 });
 
 export default router;
